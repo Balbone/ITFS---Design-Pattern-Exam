286823adc28288f3e8e247e358cefed87f8961ca
piagetgit <40717351+piagetgit@users.noreply.github.com>
2023-01-08 15:22:21 +0100

    create the Main class for composite pattern
    create the Main class for composite pattern

---
+++ java/pattern_composite/Main.java
@@ -0,0 +1,49 @@
+package designpattern.composite;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		CompoundItem store = new CompoundItem("SplendorStore");
+		
+		CompoundItem splendorCreation = new CompoundItem("splendorCreation");
+		
+		CompoundItem otherCreation = new CompoundItem("OtherCreation");
+		
+		CompoundItem accessories = new CompoundItem("Accessories");
+		
+		try {
+			store.addItem(otherCreation);
+			store.addItem(splendorCreation);
+			otherCreation.addItem(accessories);
+		} catch (SaleableItemException e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+		SaleableItem pantalone = new SaleableItem("pantalone","vestito",0.5,12.00,"42");
+		SaleableItem maglia = new SaleableItem("maglia","vestito",0.3,14.00,"XS");
+		SaleableItem cintura = new SaleableItem("cintura","accessories",0.1,5.00,"1m");
+		SaleableItem giubotto = new SaleableItem("giubotto","vestito",2,75.00,"S");
+		
+		try {
+			splendorCreation.addItem(pantalone);
+			splendorCreation.addItem(maglia);
+			
+			otherCreation.addItem(giubotto);
+			accessories.addItem(cintura);
+			
+			
+		} catch (SaleableItemException e) {
+			
+			e.printStackTrace();
+		}
+		
+		store.describe();
+		
+		
+		
+	}
+
+}
